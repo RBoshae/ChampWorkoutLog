62adad4faeb9fdf892fd17c85ffba438a117f0a8
Wanderrer <rwboshae@gmail.com>
2018-07-19 00:30:23 -0700

    Initial commit

---
+++ app/src/main/java/io/github/rboshae/champworkoutlog/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.rboshae.champworkoutlog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
